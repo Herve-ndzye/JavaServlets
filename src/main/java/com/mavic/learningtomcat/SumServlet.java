a446f6e10085c23cbf530e5497cf76254224ceaf
Herve-ndzye <hervendzye@gmail.com>
2026-01-22 12:02:08 +0200

    Adding feature to the servlet.

--- src/main/java/com/mavic/learningtomcat/SumServlet.java
+++ src/main/java/com/mavic/learningtomcat/SumServlet.java
@@ -12,10 +12,18 @@ public class SumServlet extends HttpServlet {
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int sum = num1 + num2;
-
+        ServletConfig con = getServletConfig();
+        String adminEmail = con.getInitParameter("admin_email");
+        ServletContext context = getServletContext();
+        String contextPath = context.getInitParameter("app_name");
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter out = resp.getWriter();
-        out.print("Sum is: " + sum);
+        out.println("Sum is: " + sum);
+        out.println("admin email is: " + adminEmail);
+        out.println("app name is: " + contextPath);
+
+        //use dispatch request to use radio to choose sum or add and
+        // then depending on choice you direct her to the right response
     }
 
 }
